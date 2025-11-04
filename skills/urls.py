5301609c08702c8b1f5cc05f277b8c98f9085033
Dennis Plöger <dennis.ploeger@devoba.de>
2025-11-04 10:48:11 +0100

    fix: Photo support fixed

--- skills/urls.py
+++ skills/urls.py
@@ -1,12 +1,31 @@
 import os
 from django.contrib import admin
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.urls import path, include
 from django.views.generic import RedirectView
+
+
+def media(request, file_path=None):
+    from django.conf import settings as cfg
+
+    media_root = getattr(cfg, "MEDIA_ROOT", None)
+
+    if not media_root:
+        return HttpResponseBadRequest("Invalid Media Root Configuration")
+    if not file_path:
+        return HttpResponseBadRequest("Invalid File Path")
+
+    with open(os.path.join(media_root, file_path), "rb") as doc:
+        response = HttpResponse(doc.read(), content_type="application/doc")
+        response["Content-Disposition"] = "filename=%s" % (file_path.split("/")[-1])
+        return response
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("skillsManager.urls")),
     path("", RedirectView.as_view(url="/home")),
+    path("media/<str:file_path>", media, name="media"),
     (
         path(
             "azure_auth/",
